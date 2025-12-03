a8613d8006e624919ae87e40c2962de00731b906
Zhenhua Han <hzhua201@gmail.com>
2025-12-03 11:09:56 +0800

    Use fonts.googleapis.cn fonts (#2)

--- nexdr/agents/html_creator/merge_slides.py
+++ nexdr/agents/html_creator/merge_slides.py
@@ -121,8 +121,8 @@ def build_merged_presentation(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{html.escape(title)} ({total_pages} slides)</title>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.cn/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.cn/icon?family=Material+Icons" rel="stylesheet">
     <style>
         * {{
             margin: 0;
